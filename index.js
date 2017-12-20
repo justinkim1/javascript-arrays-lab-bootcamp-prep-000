14b64d19333864f0e569d258c6ff02af50115c0a
Justin K <justin-kim1@hotmail.com>
2017-12-20 20:20:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop('Garfield')
+  return kittens
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift('Milo')
+  return kittens
+}
+function appendKitten(name){
+  [...kittens, 'Broom']
+  return kittens
+}
+function prependKitten(name){
+  return kittens
+}
+function removeLastKitten(){
+  return kittens
+}
+function removeFirstKitten(){
+  return kittens
+}

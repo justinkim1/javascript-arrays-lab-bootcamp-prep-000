ac415b0488c2830f12b631d4dc98e8db713837ef
Justin K <justin-kim1@hotmail.com>
2017-12-20 20:30:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten(name){
   return kittens
 }
 function appendKitten(name){
-  kittens = [...kittens, "Broom"]
+  kittens = [...kittens, 'Broom']
   return kittens
 }
 function prependKitten(name){
